3c397c64e3a13624b151e40aec82f05c056dce87
Michelle Labasbas <manthanein@gmail.com>
2023-04-10 04:10:25 +0000

    change output of author_details to email address link. Added safe, escape and format_html to secure site from xss

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import get_user_model
 user_model = get_user_model()
 from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 register = template.Library()
 
+ 
 @register.filter
 def author_details(author):
     if not isinstance(author, user_model):
@@ -15,4 +19,11 @@ def author_details(author):
     else:
         name = f"{author.username}"
 
-    return name
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html('{}{}{}', prefix, name, suffix)
