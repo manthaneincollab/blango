7055947b040f6908b0db89051f980dfaacbfede5
Michelle Labasbas <manthanein@gmail.com>
2023-04-10 14:21:12 +0000

    added inclusion tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,8 +4,15 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
 
 register = template.Library()
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
+
 
 @register.simple_tag(takes_context=True)
 def author_details_tag(context):
